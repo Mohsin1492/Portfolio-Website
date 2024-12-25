15d96ed477fe6b39b58925983a6d53d0ac146f27
Mohsin1492 <eliimohsin4@gmail.com>
2024-12-25 09:30:22 +0500

    Changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
                     className="logo"
                     >
                         <img 
-                        src="/images/logo.svg" 
+                        src="./images/logo.svg" 
                         width={40}
                         height={40}
                         alt="Mohsin Ali" 
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -11,37 +11,37 @@ import ProjectCard from "./ProjectCard";
 
 const works = [
     {
-      imgSrc: '/images/project-1.jpg',
+      imgSrc: './images/project-1.jpg',
       title: 'Full stack music app',
       tags: ['API', 'MVC', 'Development'],
       projectLink: '#'
     },
     {
-      imgSrc: '/images/project-2.jpg',
+      imgSrc: './images/project-2.jpg',
       title: 'Free stock photo app',
       tags: ['API', 'SPA'],
       projectLink: '#'
     },
     {
-      imgSrc: '/images/project-3.jpg',
+      imgSrc: './images/project-3.jpg',
       title: 'Recipe app',
       tags: ['Development', 'API'],
       projectLink: '#'
     },
     {
-      imgSrc: '/images/project-4.jpg',
+      imgSrc: './images/project-4.jpg',
       title: 'Real state website',
       tags: ['Web-design', 'Development'],
       projectLink: '#'
     },
     {
-      imgSrc: '/images/project-5.jpg',
+      imgSrc: './images/project-5.jpg',
       title: 'eCommerce website',
       tags: ['eCommerce', 'Development'],
       projectLink: '#'
     },
     {
-      imgSrc: '/images/project-6.jpg',
+      imgSrc: './images/project-6.jpg',
       title: 'vCard Personal portfolio',
       tags: ['Web-design', 'Development'],
       projectLink: '#'
